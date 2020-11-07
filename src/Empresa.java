73ffdba692079c22f29effd1bde9ef028c2cb66d
matias <mcoco@alumno.unlam.edu.ar>
2020-11-07 19:13:24 -0300

    commit de busqueda, eliminacion y agregado de equipos y planes

--- src/Empresa.java
+++ src/Empresa.java
@@ -10,17 +10,87 @@ public class Empresa {
 	private Set<Equipo> listaDeEquiposDisponibles;
 
 	public Empresa(String nombreDeLaEmpresa) {
-		
-		this.nombreDeLaEmpresa=nombreDeLaEmpresa;
-		
-		this.listaDeEmpleados= new HashSet<Empleado>();
-		this.listaDeClientes= new HashSet<Cliente>();
-		this.listaDePlanes= new HashSet<PlanTelefonico>();
-		this.listaDeEquiposDisponibles= new HashSet<Equipo>();
+
+		this.nombreDeLaEmpresa = nombreDeLaEmpresa;
+
+		this.listaDeEmpleados = new HashSet<Empleado>();
+		this.listaDeClientes = new HashSet<Cliente>();
+		this.listaDePlanes = new HashSet<PlanTelefonico>();
+		this.listaDeEquiposDisponibles = new HashSet<Equipo>();
 	}
-	
+
 	public Boolean agregarEmpleado(Empleado empleado) {
 		return listaDeEmpleados.add(empleado);
 	}
+
+	public Boolean eliminarEquipoViejo(Integer idDelEquipo) {
+
+		for (Equipo equipoViejo : this.listaDeEquiposDisponibles) {
+
+			if (equipoViejo.getCodigoDelProducto().equals(idDelEquipo)) {
+				return this.listaDeEquiposDisponibles.remove(equipoViejo);
+			}
+		}
+		return false;
+	}
 	
+	public Boolean agregarEquipo(Equipo equipoNuevo) {
+		return this.listaDeEquiposDisponibles.add(equipoNuevo);
+	}
+
+	public Boolean agregarPlan(PlanTelefonico planNuevo) {
+		return this.listaDePlanes.add(planNuevo);
+	}
+	
+	public HashSet<PlanTelefonico> getPlanesVigentes() {
+		return (HashSet<PlanTelefonico>) this.listaDePlanes;
+	}
+	
+	public HashSet<Equipo> getEquiposVigentes() {
+		return (HashSet<Equipo>) this.listaDeEquiposDisponibles;
+	}
+	
+	public Equipo buscarEquipo(Equipo equipoDeseado) {
+		
+		for(Equipo equipoActual:this.listaDeEquiposDisponibles) {
+			
+			if(equipoActual.equals(equipoDeseado)) {
+				return equipoActual;
+			}
+		}
+		
+		return null;
+	}
+	
+	public PlanTelefonico buscarPlan(Integer IdplanABuscar) {
+		
+		for(PlanTelefonico planBuscado : this.listaDePlanes) {
+			
+			if(planBuscado.getId().equals(IdplanABuscar)) {
+				return planBuscado;
+			}
+		}
+		return null;
+	}
+	
+	public Boolean ModificarElPrecioDeUnPlanTelefonico(Double precioActualizado, Integer IdpPlanQueDeseoModificar) {
+		
+		if(this.buscarPlan(IdpPlanQueDeseoModificar)!=null) {
+			
+			this.buscarPlan(IdpPlanQueDeseoModificar).setPrecio(precioActualizado);
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean eliminarPlan(Integer id) {
+		
+		for(PlanTelefonico planActual: this.listaDePlanes) {
+			
+			if(this.buscarPlan(id)!=null) {
+				return this.listaDePlanes.remove(this.buscarPlan(id));
+			}
+		}	
+		return false;
+	}
 }
--- test/tests.java
+++ test/tests.java
@@ -6,15 +6,89 @@ public class tests {
 
 	@Test
 	public void queSePuedaAgregarUnEmpleado() {
-		
-		Empresa empresa = new Empresa ("sarasa");
-		
+
+		Empresa empresa = new Empresa("sarasa");
+
 		Empleado matias = new Tecnico("Zona 1", 1, 123, 50.0, 4175758, "Hernan");
-		
+
 		Boolean ve = empresa.agregarEmpleado(matias);
 		Boolean va = true;
-		
+
 		assertEquals(ve, va);
 	}
 
+	@Test
+	public void queSePuedaAgregarUnEquipo() {
+
+		Empresa empresa = new Empresa("sarasa");
+		Equipo motorola = new Equipo("J7", 123, 40000.0);
+
+		empresa.agregarEquipo(motorola);
+		Integer valorEsperado = 1;
+		Integer valorObtenido = empresa.getEquiposVigentes().size();
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queSePuedaEliminarUnEquipo() {
+
+		Empresa empresa = new Empresa("sarasa");
+		Equipo motorola = new Equipo("J7", 123, 40000.0);
+
+		empresa.agregarEquipo(motorola);
+
+		Integer valorEsperado = 0;
+		empresa.eliminarEquipoViejo(123);
+		Integer valorObtenido = empresa.getEquiposVigentes().size();
+		assertEquals(valorEsperado, valorObtenido);
+
+	}
+
+	@Test
+	public void queSePuedaBuscarUnEquipo() {
+
+		Empresa empresa = new Empresa("sarasa");
+		Equipo motorola = new Equipo("J7", 123, 40000.0);
+
+		empresa.agregarEquipo(motorola);
+		empresa.buscarEquipo(motorola);
+
+		Equipo equipoEsperado = motorola;
+		Equipo equipoObtenido = empresa.buscarEquipo(motorola);
+
+		assertEquals(equipoEsperado, equipoObtenido);
+	}
+
+	@Test
+	public void queSePuedaBuscarUnPlan() {
+
+		Empresa empresa = new Empresa("sarasa");
+		PlanTelefonico planEsperado = new PlanTelefonico(1000.0, 10, "Plan Basico");
+		empresa.agregarPlan(planEsperado);
+		PlanTelefonico PlanObtenido = empresa.buscarPlan(10);
+		assertEquals(planEsperado, PlanObtenido);
+	}
+	
+	@Test
+	public void queSePuedaAgregarUnPlan() {
+
+		Empresa empresa = new Empresa("sarasa");
+		PlanTelefonico plan = new PlanTelefonico(1000.0, 10, "Plan Basico");
+		empresa.agregarPlan(plan);
+		Integer resultadoEsperado=1;
+		Integer resultadoObtenido=empresa.getPlanesVigentes().size();
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	
+	@Test
+	public void queSePuedaEliminarUnPlan() {
+
+		Empresa empresa = new Empresa("sarasa");
+		PlanTelefonico plan = new PlanTelefonico(1000.0, 10, "Plan Basico");
+		empresa.agregarPlan(plan);
+		Integer resultadoEsperado=0;
+		empresa.eliminarPlan(10);
+		Integer resultadoObtenido=empresa.getPlanesVigentes().size();
+		assertEquals(resultadoEsperado, resultadoObtenido);	
+	}
 }
